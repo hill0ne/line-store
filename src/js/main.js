05fa1d88061d01feac638f5e5ddb44a7697f9730
jiwon <jeanj531@gmail.com>
2023-03-18 15:42:55 +0900

    swiper 스타일 작업

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,15 @@
 let swiper = new Swiper(".swiper", {
-    navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
-    autoplay: { 
-        delay: 5000, 
-        disableOnInteraction: false, 
-      },
-    // loop: true,
-  });
+  autoplay: {
+    delay: 3000,
+  },
+  loop: true,
+});
